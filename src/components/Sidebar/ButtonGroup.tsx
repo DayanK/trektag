0f58a0b3f69c5e10bf81d5ed8e71c486bc0e1eef
Maxim Kemajou <mckemajou@live.fr>
2024-11-22 11:56:00 +0100

    refactoring Button group

--- src/components/Sidebar/ButtonGroup.tsx
+++ src/components/Sidebar/ButtonGroup.tsx
@@ -1,14 +1,23 @@
 import Button from "../Button";
 
+
 const ButtonGroup = () => {
+  const secondaryButtons: string[] = [
+    "Mark all as complete",
+    "Mark all as incomplete",
+    "Reset to initial",
+    "Remove all items"
+  ];
+
   return (
     <section className="button-group">
-        <Button type="secondary">Mark all as complete</Button>
-        <Button type="secondary">Mark all as incomplete</Button>
-        <Button type="secondary">Reset to initial</Button>
-        <Button type="secondary">Remove all items</Button>             
+      {secondaryButtons.map((text) => (
+        <Button key={text} type="secondary">
+          {text}
+        </Button>
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default ButtonGroup
+export default ButtonGroup;
